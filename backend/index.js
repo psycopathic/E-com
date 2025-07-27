a4e814a1048159c661a23e5467bf8487699cebd3
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-07-27 22:15:43 +0530

    this is the cart

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(cors(
         credentials:true
     }
 ));
-app.use(express.json({limit:"10mb"}));
+app.use(express.json({limit:"100mb"}));
 app.use(cookieParser());
 
 app.use('/api/auth',authRouter);
